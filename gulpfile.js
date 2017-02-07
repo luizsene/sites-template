93d01df0002b989ed90eac9d407b01a5f9007c02
1fabiopereira <fabio.pereira.gti@hotmail.com>
2017-02-07 13:09:38 -0200

    Add fonts, gulp minify css, gulp minify js :rocket:

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,46 @@
 const gulp = require('gulp');
 var concat = require('gulp-concat');
 const minify = require('gulp-minify');
+const minifyCss = require('gulp-minify-css');
+const rename = require('gulp-rename');
+const strip = require('gulp-strip-comments');
 
 var paths = {
-    css: [],
+    css: [
+        './css/*.css'
+    ],
+
     js: [
-        //'./js/jquery-min.js',
-        './js/jquery.appear.js',
+        './js/jquery.min.js',
+        './js/jquery.appear.min.js',
         './js/jquery.magnific-popup.min.js',
         './js/jquery.themepunch.revolution.min.js',
         './js/jquery.themepunch.tools.min.js',
-         './js/bootstrap.min.js ',
+        './js/bootstrap.min.js ',
         './js/revolution.extension.layeranimation.min.js',
         './js/revolution.extension.slideanims.min.js',
         './js/revolution.extension.parallax.min.js',
         './js/imagesloaded.pkgd.min.js',
         './js/owl.carousel.min.js',
-        './js/main.js'
+        './js/main.min.js'
     ]
 };
 
-gulp.task('minify', function () {
+gulp.task('minify:js', function () {
     return gulp.src(paths.js)
         .pipe(concat('bundle.js'))
-        .pipe(minify({
-            ext: {
-                src: '.js',
-                min: '.min.js'
-            },
-            exclude: [],
-            ignoreFiles: ['-min.js']
-        }))
+        .pipe(strip())
         .pipe(gulp.dest('./bundle/'));
+});
+
+
+gulp.task('minify:css', function(done) {
+    gulp.src(paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('./bundle/'))
+        .on('end', done);
 });
