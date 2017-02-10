98261b6d956e442c5d1acc8967040416724b20a5
1fabiopereira <fabio.pereira.gti@hotmail.com>
2017-02-10 17:06:38 -0200

    Alterações

--- routes/common.js
+++ routes/common.js
@@ -3,8 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const path = require("path");
+const UserAgent = require('../utils/userAgent');
 
-router.get('/', (req, res) =>{
+router.get('/', [UserAgent.getUserAgent], (req, res) =>{
     res.sendFile(path.join(__dirname + '/../views/index.html'));
 });
 
--- utils/userAgent.js
+++ utils/userAgent.js
@@ -1,35 +1,13 @@
 'use strict';
 
+const userAgent = require('express-useragent');
+
 class UserAgent {
 
     static getUserAgent(req, res, next){
-
-        const ua = req.headers['user-agent'];
-        const $ ={};
-
-        if (/mobile/i.test(ua))
-            $.Mobile = true;
-
-        if (/like Mac OS X/.test(ua)) {
-            $.iOS = /CPU( iPhone)? OS ([0-9\._]+) like Mac OS X/.exec(ua)[2].replace(/_/g, '.');
-            $.iPhone = /iPhone/.test(ua);
-            $.iPad = /iPad/.test(ua);
-        }
-
-        if (/Android/.test(ua))
-            $.Android = /Android ([0-9\.]+)[\);]/.exec(ua)[1];
-
-        if (/webOS\//.test(ua))
-            $.webOS = /webOS\/([0-9\.]+)[\);]/.exec(ua)[1];
-
-        if (/(Intel|PPC) Mac OS X/.test(ua))
-            $.Mac = /(Intel|PPC) Mac OS X ?([0-9\._]*)[\)\;]/.exec(ua)[2].replace(/_/g, '.') || true;
-
-        if (/Windows NT/.test(ua))
-            $.Windows = /Windows NT ([0-9\._]+)[\);]/.exec(ua)[1];
-
-        console.log('User Agent: ', $);
-
+        const source = req.headers['user-agent'];
+        const agent = userAgent.parse(source);
+        console.log('User Agent: ', agent);
         next();
     }
 
